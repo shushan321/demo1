1bf59df55a6ac5e10fb750fdb764aab7d8eae4c9
shushan321 <arangenishishen@foxmail.com>
2026-07-15 12:20:04 +0800

    Add src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import VenueDetail from './pages/VenueDetail';
+import Payment from './pages/Payment';
+import Orders from './pages/Orders';
+import Profile from './pages/Profile';
+import Timer from './pages/Timer';
+import BottomNav from './components/BottomNav';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/venue/:id" element={<VenueDetail />} />
+        <Route path="/payment" element={<Payment />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/timer" element={<Timer />} />
+      </Routes>
+      <BottomNav />
+    </BrowserRouter>
+  );
+}
+
+export default App;
